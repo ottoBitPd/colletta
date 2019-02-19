4aa5c35203c6f2c21e9b975e756dd32502f93798
Perry15 <peron.giovanni@gmail.com>
2019-02-19 11:19:38 +0100

    ristrutturazione controller

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const express = require('express');
 var app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-var UrlManager = require('./js/urlManager.js');
-var u = new UrlManager();
 
-var server = u.start(app).listen(8080, function () {
+
+var Controller = require('./js/controller.js');
+var c = new Controller();
+
+
+var server = c.start(app).listen(8080, function () {
     var host = "127.0.0.1";
     var port = "8080";
     console.log("Example app listening at http://%s:%s", host, port)

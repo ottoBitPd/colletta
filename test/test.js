5f7812a11a457569d60048059ff4247b4e44c660
benny1693 <benedetto.cosentino@studenti.unipd.it>
2019-03-01 17:35:28 +0100

    Tentativo di analisi statica con sonarqube

--- test/test.js
+++ test/test.js
@@ -1,10 +1,7 @@
-
 var Exercise = require('../src/js/controller/Exercise.js');
 var expect = require('chai').expect;
 
 describe('#getSentence()', function() {
-
-
     context('sentence passed', function() {
         var obj= new Exercise();
         obj.setSentence("ciao amico");

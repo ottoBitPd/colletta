148a55b59e1859716bfbe1bc4412fcb3e0f51d1e
Giovanni Bergo <giovanni.bergo@icloud.com>
2019-04-23 16:56:31 +0200

    DatabaseManagerTest

--- code/test/model/DatabaseManager/DatabaseClassManagerTest.ts
+++ code/test/model/DatabaseManager/DatabaseClassManagerTest.ts
@@ -8,22 +8,75 @@ import {FirebaseClassManager} from "../../../src/ts/model/Firebase/FirebaseClass
 
 describe('DatabaseClassManager', function() {
 
+    let prova :any;
+    let test:FirebaseClassManager;
     const stubObj= sinon.stubObject;
+
+    beforeEach(function () {
+         prova= new Class("1","Benedetto","Ciao","111",["st1"],["es1"]);
+         test= new FirebaseClassManager();
+    });
 
     describe('DatabaseClassManager.insert()', function () {
         it('should return the insert obj in database', async function() {
 
-            let prova= new Class("1","Benedettolone","boh","111",["st1"],["es1"]);
-            let test= new FirebaseClassManager();
+            const testStub= stubObj<FirebaseClassManager>(test,{insert:true});
 
-
-            const testStub= stubObj<FirebaseClassManager>(test,{insert:'true'});
-
-            expect( await testStub.insert(prova)).to.equal('true');
-
+            expect( await testStub.insert(prova)).to.equal(true);
 
         });
+    });
 
+
+    describe('DatabaseClassManager.remove()', function () {
+        it('should return the remove obj in database', async function() {
+
+            const testStub= stubObj<FirebaseClassManager>(test,{remove:true});
+
+            expect(testStub.remove('ciao')).to.equal(true);
+
+        });
+    });
+
+
+    describe('DatabaseClassManager.read()', function () {
+        it('should return read obj in database', async function() {
+
+            const testStub= stubObj<FirebaseClassManager>(test,{read:true});
+
+            expect(testStub.read('ciao')).to.equal(true);
+
+        });
+    });
+
+    describe('DatabaseClassManager.search()', function () {
+        it('should return search obj in database', async function() {
+
+            const testStub= stubObj<FirebaseClassManager>(test,{search:true});
+
+            expect(testStub.search('ciao')).to.equal(true);
+
+        });
+    });
+
+    describe('DatabaseClassManager.update()', function () {
+        it('should return update database', async function() {
+
+            const testStub= stubObj<FirebaseClassManager>(test,{update:true});
+
+            expect(testStub.update("/data/classes/-Ld9ae0AXaB9_KbZ-sGJ/exercises/0","es3")).to.equal(true);
+
+        });
+    });
+
+    describe('DatabaseClassManager.elements()', function () {
+        it('should return elements database', async function() {
+
+           const testStub= stubObj<FirebaseClassManager>(test,{elements:true});
+
+            expect(testStub.elements()).to.equal(true);
+
+        });
     });
 
 });

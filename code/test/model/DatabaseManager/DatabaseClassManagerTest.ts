00bf48e46df268638357d309e2b2780e9b5f7e21
Giovanni Bergo <giovanni.bergo@icloud.com>
2019-04-24 12:30:05 +0200

    DatabaseClassTest

--- code/test/model/DatabaseManager/DatabaseClassManagerTest.ts
+++ code/test/model/DatabaseManager/DatabaseClassManagerTest.ts
@@ -1,90 +1,104 @@
 import {expect} from 'chai';
 import 'mocha';
-//import * as sinon from "ts-sinon";
-
-import * as sinon from "sinon";
 
 import {Class} from "../../../src/ts/model/Data/Class";
 
-import {FirebaseClassManager} from "../../../src/ts/model/Firebase/FirebaseClassManager";
+import {DatabaseClassManager} from "../../../src/ts/model/DatabaseManager/DatabaseClassManager";
+
 
 describe('DatabaseClassManager', function() {
 
     let prova :any;
-    let test:FirebaseClassManager;
-  //  const stubObj= sinon;
+
+    let test1=new DatabaseClassManager();
 
     beforeEach(function () {
          prova= new Class("1","Benedetto","Ciao","111",["st1"],["es1"]);
-         test= new FirebaseClassManager();
+
+         //@ts-ignore
+         test1.firebaseManager={
+             async insert(obj:Class):Promise<boolean>
+             {
+                 return true;
+             },
+
+             async remove(obj:string):Promise<boolean>
+             {
+                 return true;
+             },
+             async read(id:string):Promise<any>
+             {
+                 return "classe"
+             },
+             async search(id:string):Promise<string>
+             {
+                 return "key"
+             },
+             async update(path:string, value: any)
+             {
+                 return true;
+             },
+             async elements() : Promise<Map<string, string>> {
+                 let now=new Map<string,string>();
+                 now.set("key","teacherID");
+                 console.log(now);
+                 return now;
+             }
+
+         }
+
     });
 
     describe('DatabaseClassManager.insert()', function () {
         it('should return the insert obj in database', async function() {
 
-            var stub =  sinon.spy(test,'insert');
-
-            var r = await test.insert(prova);
-
-            r=stub.calledWith(prova);
-
-            stub.restore();
-
-            expect(r).to.equal(true);
+            expect(await test1.insert(prova)).to.equal(true);
 
         });
     });
-/*
+
 
     describe('DatabaseClassManager.remove()', function () {
         it('should return the remove obj in database', async function() {
 
-            const testStub= stubObj<FirebaseClassManager>(test,{remove:true});
-
-            expect(testStub.remove('ciao')).to.equal(true);
+            expect(await test1.remove('ciao')).to.equal(true);
 
         });
     });
 
 
-    describe('DatabaseClassManager.read()', function () {
-        it('should return read obj in database', async function() {
+        describe('DatabaseClassManager.read()', function () {
+            it('should return read obj in database', async function() {
 
-            const testStub= stubObj<FirebaseClassManager>(test,{read:true});
+                expect(await test1.read('ciao')).to.equal("classe");
 
-            expect(testStub.read('ciao')).to.equal(true);
-
+            });
         });
-    });
 
-    describe('DatabaseClassManager.search()', function () {
-        it('should return search obj in database', async function() {
+            describe('DatabaseClassManager.search()', function () {
+                it('should return search obj in database', async function() {
 
-            const testStub= stubObj<FirebaseClassManager>(test,{search:true});
+                    expect(await test1.search('ciao')).to.equal("key");
 
-            expect(testStub.search('ciao')).to.equal(true);
 
-        });
-    });
+                });
+            });
 
-    describe('DatabaseClassManager.update()', function () {
-        it('should return update database', async function() {
+            describe('DatabaseClassManager.update()', function () {
+                it('should return update database', async function() {
 
-            const testStub= stubObj<FirebaseClassManager>(test,{update:true});
+                    expect(await test1.update("/data/classes/-Ld9ae0AXaB9_KbZ-sGJ/exercises/0","es3")).to.equal(true);
 
-            expect(testStub.update("/data/classes/-Ld9ae0AXaB9_KbZ-sGJ/exercises/0","es3")).to.equal(true);
+                });
+            });
 
-        });
-    });
+            describe('DatabaseClassManager.elements()', function () {
+                it('should return elements database', async function() {
+let now=new Map<string,string>();
+now.set("key","teacherID");
+                    expect(await test1.elements()).eql(now);
 
-    describe('DatabaseClassManager.elements()', function () {
-        it('should return elements database', async function() {
+                });
+            });
 
-           const testStub= stubObj<FirebaseClassManager>(test,{elements:true});
-
-            expect(testStub.elements()).to.equal(true);
-
-        });
-    });
-*/
 });
